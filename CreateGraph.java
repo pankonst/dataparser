17d9bfeee66352370379cdf9db62a1fb089ecd99
oracle <oracle@bigdatalite.localdomain>
2017-07-31 09:38:40 -0400

    initial java files commit

--- CreateGraph.java
+++ CreateGraph.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class CreateGraph {
-    public static void main (String[] args) throws IOException{
+    public static void main (String[] args){
         Stopwatch timer = new Stopwatch().start();
         ImportToOracleNoSQL imp = new ImportToOracleNoSQL();
         
